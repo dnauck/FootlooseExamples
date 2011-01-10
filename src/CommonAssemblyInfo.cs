0e8783f144b195e3b2d196dc84702e08598944f9
Daniel Nauck <d.nauck@nauck-it.de>
2011-01-10 20:35:05 +0100

    Update to Footloose 0.1.34.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompanyAttribute("Nauck IT KG")]
 [assembly: AssemblyProductAttribute("Footloose Examples")]
-[assembly: AssemblyCopyrightAttribute("Copyright © 2008 - 2010 Nauck IT KG")]
+[assembly: AssemblyCopyrightAttribute("Copyright © 2008 - 2011 Nauck IT KG")]
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyVersionAttribute("1.0.0.0")]
 [assembly: AssemblyFileVersionAttribute("1.0.0.0")]
